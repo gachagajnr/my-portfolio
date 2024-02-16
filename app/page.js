5e0228255efd7d7f04a8a54a12ac33b3991fb038
gachagajnr <williampius17@users.noreply.github.com>
2024-02-16 12:37:54 +0300

    add rentaspace

--- app/page.js
+++ app/page.js
@@ -188,10 +188,8 @@ export default function Home() {
           <h3 className="text-xs font-mono">Overview</h3>
           <p>Node JS Web Server</p> */}
         </div>
-        
       </div>
       <div className=" max-w-5xl w-full items-center   justify-between lg:flex md:flex-row ">
-        
         <div className="flex-col p-2">
           <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="https://michoro-postgres.vercel.app/">
@@ -234,10 +232,53 @@ export default function Home() {
           <p>Art Based Ecommerce Website</p> */}
         </div>
       </div>
+      <div className=" max-w-5xl w-full items-center   justify-between lg:flex md:flex-row ">
+        <div className="flex-col p-2">
+          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="https://michoro-postgres.vercel.app/">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Rentaspace
+              </h5>
+            </a>
+            <p class="mb-2 font-mono text-sm text-gray-700 dark:text-gray-400">
+              Overview
+            </p>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Rentals Listing System
+            </p>
+            <a
+              href="https://michoro-postgres.vercel.app/"
+              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Learn More
+              <svg
+                class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </div>
+          {/* <div className=" pb-8 font-bold text-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            Michoro Art Website
+          </div>
+          <h3 className="text-xs font-mono">Overview</h3>
+          <p>Art Based Ecommerce Website</p> */}
+        </div>
+      </div>
       <h1 className="my-8 px-2 font-extralight text-3xl">Contact Me </h1>
       <p>+254 706-335-880</p>
       <p>williampius17@gmail.com</p>
-      <p>https://www.linkedin.com/in/muthomi-gachaga-8b98a1121/</p>
+      <p>https://www.linkedin.com/in/muthomi-gachaga/</p>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
     </main>
