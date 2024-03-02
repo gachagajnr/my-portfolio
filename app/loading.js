780e22e06809e104a6fc52322e56580950004299
gachagajnr <williampius17@gmail.com>
2024-03-02 12:02:24 +0300

    ad loading

---
+++ app/loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex items-center gap-1 justify-center text-center h-80">
+      <div className=" loading loading-dots loading-md text-secondary "></div>
+      <p className="text-center">Pius Gachaga</p>
+    </div>
+  );
+};
+
+export default loading;
