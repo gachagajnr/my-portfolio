5b53c5fe05c0e4b481e5fbd2361fed6cab9d2c61
gachagajnr <williampius17@gmail.com>
2024-03-02 04:25:53 +0300

    morestyling

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import Link from "next/link";
 const stack = [
   "Flutter",
   "Feathers JS",
@@ -51,8 +51,8 @@ export default function Home() {
         am excited to contribute my skills to innovative projects that push the
         boundaries of technology.
       </p>
-      <h1 className="py-2 px-2 font-bold text-3xl">Tech Stack</h1>
-      <div className="menu menu-horizontal gap-2  justify-center">
+      <h1 className="py-2 px-2 font-bold text-4xl">Tech Stack</h1>
+      <div className="menu menu-horizontal gap-2 py-2  justify-center">
         {stack.map((item) => {
           return (
             <h2 key={item} className="btn btn-sm rounded-md">
@@ -61,222 +61,70 @@ export default function Home() {
           );
         })}
       </div>
-      <h1 className="my-8 px-2 font-extralight text-3xl">Projects </h1>
-      <div className=" max-w-5xl w-full items-center   justify-between lg:flex md:flex-row ">
-        <div className="flex-col p-2">
-          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Mahali Kenya Mobile App
-              </h5>
-            </a>
-            <p class="mb-2 font-mono text-sm text-gray-700 dark:text-gray-400">
-              Overview
-            </p>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Flutter Based Cross Platform Mobile App.
-            </p>
-            <a
-              href="#"
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Download
-              <svg
-                class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
-          </div>
+      <h1 className="my-8 px-2 font-bold text-4xl">Projects </h1>
 
-          {/* <div className=" pb-8 font-bold text-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-            MahaliKenya Mobile App
+      <div className="flex flex-row flex-wrap  justify-center  gap-4 items-center">
+        <div className="card  bg-base-100 shadow-xl glass">
+          <div className="card-body">
+            <h2 className="card-title">Mahali Kenya Mobile App</h2>
+            <p>Flutter Cross Platform Mobile App.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Visit</button>
+            </div>
           </div>
-          <h3 className="text-xs font-mono ">Overview</h3>
-          <p>Flutter Based Mobile App</p> */}
         </div>
-        <div className="flex-col p-2">
-          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="https://michoro-postgres.vercel.app/">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Michoro Art Website
-              </h5>
-            </a>
-            <p class="mb-2 font-mono text-sm text-gray-700 dark:text-gray-400">
-              Overview
-            </p>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Node JS & React Js Art Based Ecommerce Website
-            </p>
-            <a
-              href="https://michoro-postgres.vercel.app/"
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Learn More
-              <svg
-                class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
+        <div className="card bg-base-100 shadow-xl glass">
+          <div className="card-body">
+            <h2 className="card-title"> Michoro Art Ecommerce</h2>
+            <p>Express & React Js Art Ecommerce Web Application</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">View</button>
+            </div>
           </div>
-          {/* <div className=" pb-8 font-bold text-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-            Michoro Art Website
-          </div>
-          <h3 className="text-xs font-mono">Overview</h3>
-          <p>Art Based Ecommerce Website</p> */}
         </div>
-        <div className="flex-col p-2">
-          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="https://www.mahalikenya.com">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Mahali Kenya Web App
-              </h5>
-            </a>
-            <p class="mb-2 font-mono text-sm text-gray-700 dark:text-gray-400">
-              Overview
-            </p>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Node JS Landing Page & Web Server
-            </p>
-            <a
-              href="https://www.mahalikenya.com"
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read More
-              <svg
-                class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
+        <div className="card bg-base-100 shadow-xl glass">
+          <div className="card-body">
+            <h2 className="card-title"> Wirerent</h2>
+            <p>Node & React Js Rental Management System</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">View</button>
+            </div>
           </div>
-          {/* <div className=" pb-8 font-bold text-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-            Mahali Kenya Web App
-          </div>
-          <h3 className="text-xs font-mono">Overview</h3>
-          <p>Node JS Web Server</p> */}
         </div>
-      </div>
-      <div className=" max-w-5xl w-full items-center   justify-between lg:flex md:flex-row ">
-        <div className="flex-col p-2">
-          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="https://michoro-postgres.vercel.app/">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Wirerent
-              </h5>
-            </a>
-            <p class="mb-2 font-mono text-sm text-gray-700 dark:text-gray-400">
-              Overview
-            </p>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Node JS & React Js Based Rental Management System
-            </p>
-            <a
-              href="https://michoro-postgres.vercel.app/"
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Learn More
-              <svg
-                class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
+        <div className="card  bg-base-100 shadow-xl glass">
+          <div className="card-body">
+            <h2 className="card-title">Rentaspace</h2>
+            <p> Next JS Properties Listing Full Stack Web Application</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">View</button>
+            </div>
           </div>
-          {/* <div className=" pb-8 font-bold text-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-            Michoro Art Website
-          </div>
-          <h3 className="text-xs font-mono">Overview</h3>
-          <p>Art Based Ecommerce Website</p> */}
-        </div>
-        <div className="flex-col p-2">
-          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="https://michoro-postgres.vercel.app/">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Rentaspace
-              </h5>
-            </a>
-            <p class="mb-2 font-mono text-sm text-gray-700 dark:text-gray-400">
-              Overview
-            </p>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Rentals Listing System
-            </p>
-            <a
-              href="https://michoro-postgres.vercel.app/"
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Learn More
-              <svg
-                class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
-          </div>
-          {/* <div className=" pb-8 font-bold text-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-            Michoro Art Website
-          </div>
-          <h3 className="text-xs font-mono">Overview</h3>
-          <p>Art Based Ecommerce Website</p> */}
         </div>
       </div>
 
-      <h1 className="my-8 px-2 font-extralight text-3xl">Contact Me </h1>
-      <p>+254 706-335-880</p>
-      <p>williampius17@gmail.com</p>
-      <p>https://www.linkedin.com/in/muthomi-gachaga/</p>
+      <h1 className="my-8 px-2 font-bold text-4xl">Reach Out</h1>
+      <div className="py-2 menu menu-horizontal gap-3">
+        <p className="text-center font-semibold text-xl">
+          <Link href="https://github.com/gachagajnr">Github</Link>
+        </p>
+        <p className="text-center font-semibold text-xl">
+          <Link href="https://www.linkedin.com/in/muthomi-gachaga">
+            LinkedIn
+          </Link>
+        </p>
+        <p className="text-center font-semibold text-xl">
+          <Link href="https://www.facebook.com/pius.muthomi.967/">
+            Facebook
+          </Link>
+        </p>
+      </div>
+      <p className="text-center font-semibold text-xl">
+        <span className="text-sm">Email: </span> williampius17@gmail.com
+      </p>
+      <p className="text-center font-semibold text-xl">
+        <span className="text-sm">Mobile: </span>
+        (+254) 706 335 880
+      </p>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
     </main>
